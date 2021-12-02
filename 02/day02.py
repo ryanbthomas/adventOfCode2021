12a3e5ff0aaabc0862a4f22f0f8ad28e685fbb11
Ryan Thomas <rthomas2003@gmail.com>
2021-12-02 09:38:15 -0500

    completed day 02 part 2

--- 02/day02.py
+++ 02/day02.py
@@ -9,6 +9,17 @@ def move_sub(start, direction, units):
     
     return (start[0], start[1] + sign * units)
 
+def move_sub2(start, direction, units):
+    #start is now (hort, vert, aim)
+    if direction == "forward":
+        return (start[0] + units, start[1] + start[2]*units, start[2])
+    
+    sign = 1
+    if direction == "up":
+        sign = -1
+    
+    return (start[0], start[1], start[2] + sign * units)
+
 
 def load_commands(input_file):
     f = open(input_file, "r")
@@ -26,6 +37,15 @@ def load_commands(input_file):
 
     return directions, units
 
+def move_to_final_pos(move, start_pos, directions, units):
+    
+    for i in range(len(directions)):
+        new_pos = move(start_pos, directions[i], units[i])
+        print("Move sub from {} {} {} units -> {}".format(start_pos, directions[i], units[i], new_pos))        
+        start_pos = new_pos
+    
+    return(new_pos)
+
 if __name__ == "__main__":
 
     print("Check to see if load_commands works")
@@ -35,10 +55,10 @@ if __name__ == "__main__":
     print("units: {}".format(u))
 
     print("Check to see if we can move the sub")
-    pos = (0, 0)
-    for i in range(len(d)):
-        new_pos = move_sub(pos, d[i], u[i])
-        print("Move sub from {} {} {} units -> {}".format(pos, d[i], u[i], new_pos))        
-        pos = new_pos
-    
+    pos = move_to_final_pos(move_sub, (0, 0), d, u)
     print ("The final position is {}".format(pos))
+
+    print("Check to see if we can move the sub using method 2")
+    pos = move_to_final_pos(move_sub2, (0, 0, 0), d, u)
+    print ("The final position is {}".format(pos))
+    
--- 02/sol.py
+++ 02/sol.py
@@ -3,16 +3,15 @@ import day02
 
 (d, u) = day02.load_commands("input")
 
-print("directions: {}".format(d))
-print("units: {}".format(u))
 
 print("Check to see if we can move the sub")
-pos = (0, 0)
-for i in range(len(d)):
-    new_pos = day02.move_sub(pos, d[i], u[i])
-    print("Move sub from {} {} {} units -> {}".format(pos, d[i], u[i], new_pos))        
-    pos = new_pos
- 
+pos = day02.move_to_final_pos(day02.move_sub, (0, 0), d, u) 
+print ("The final position is {}".format(pos))
+
+print("Part 1 answer is {} * {} = {}".format(pos[0], pos[1], pos[0]*pos[1]))
+
+print("Let's move the sub using correct method")
+pos = day02.move_to_final_pos(day02.move_sub2, (0, 0, 0), d, u) 
 print ("The final position is {}".format(pos))
 
 print("Part 1 answer is {} * {} = {}".format(pos[0], pos[1], pos[0]*pos[1]))
