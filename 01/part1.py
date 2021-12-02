78a17403dc41dc1f09bfe075dca58dda741fbaf9
Ryan Thomas <rthomas2003@gmail.com>
2021-12-02 00:07:21 -0500

    combining parts into one file

--- 01/part1.py
+++ 01/part1.py
@@ -7,4 +7,10 @@ input= list(map(int, f.read().split("\n")))
 
 f.close()
 
+print("Part 1")
 print("The number of depth increases is {}".format(day01.find_depth_increases(input)))
+
+print("Part 2")
+rolling_avgs = day01.create_rolling_window(input)
+
+print("The number of depth increases is {}".format(day01.find_depth_increases(rolling_avgs)))
