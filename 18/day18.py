d555a9861d211ccf2867845d9ace68783035c44b
Ryan Thomas <rthomas2003@gmail.com>
2021-12-22 10:00:18 -0500

    starting on day18

---
+++ 18/day18.py
@@ -0,0 +1,31 @@
+
+class sfnum:
+
+    def __init__(self, x):
+        self.num = x        
+
+    def reduce(x):
+        # steps to reduce snailfish num
+
+        return x
+    
+    def from_str(s):
+        # parse nested array from str
+        # 
+        sfn = sfnum([])
+
+        return sfn
+    
+    def __add__(self, o):
+        # return a new object
+        sfn = sfnum.reduce(sfnum([]))
+
+        return sfn
+
+    def magnitude(x):
+        if isinstance(x, int):
+            return x
+
+        return 3 * sfnum.magnitude(x[0]) + 2 * sfnum.magnitude(x[1]) 
+
+
